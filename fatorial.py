307ab118c2266a1856e6074b285d473ee3ca19f9
jpdantas1000 <eldantecoloco123@gmail.com>
2024-11-16 09:39:28 -0300

    fatorial

---
+++ fatorial.py
@@ -0,0 +1,11 @@
+def fatorial(n: int) -> int:
+    if n == 0:
+        return 1
+    else:
+        return n * fatorial(n-1)
+
+
+
+numero: int = int(input("Informe um numero inteiro: \n"))
+resultado: int = fatorial(numero)
+print(resultado)
